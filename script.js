fb0c2a6d7bed87832766828a49a38df8467ac9fb
Emmanuel <himkolee40@gmail.com>
2024-01-14 22:16:35 +0100

    change navbar borderBottom

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	window.addEventListener('scroll', function() {
 		if (window.scrollY === 0) {
 			navbar.style.backgroundColor = 'transparent';
+			navbar.style.borderBottom = 'none';
 		} else {
 			navbar.style.backgroundColor = 'var(--main-color)';
+			navbar.style.borderBottom = '5px solid var(--tetiary-color)';
 		}
 	});
 });
